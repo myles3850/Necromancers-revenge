6f3d6a8c78f27836f368bc695ab833b92879d779
alex <thefallen24939@gmail.com>
2019-08-03 22:44:52 +0100

    start screen now exists

---
+++ Start.py
@@ -0,0 +1,5 @@
+print('hi welcome')
+print('beign stuck on a nausiating disk gets dull so if youd talk to me i can take you on an adventure')
+if input():
+    print('thanks i apreciate it')
+print('now lets go')
